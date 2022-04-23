909d61af991f47773966532d743a72b4c94fae7a
Abdurrahmon Badmus <abdurrahmonbadmus.ab@gmail.com>
2022-04-23 22:07:21 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lessThan100(22, 15));
+            Console.WriteLine(lessThan100(83, 34));
+            Console.WriteLine(lessThan100(3, 77));
+            Console.ReadLine();
+            //int result > (x + y);
+        }
+
+      public static bool lessThan100(int x, int y)
+        {
+            return (x + y < 100);
+        }
+    }
+}
